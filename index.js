5f915c3c8c10fb30749d463d51e69c6ac75676c0
EnzoAlv <alvarengaenzo2005@gmail.com>
2025-06-18 23:58:05 -0300

    index atualizado com uma mensagem de boas vindas no deploy

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const produtoRoutes = require('./routes/produtoRoutes');
@@ -5,10 +6,15 @@ const produtoRoutes = require('./routes/produtoRoutes');
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://dbUser:dbUserPassword@cluster0.pwlmzmc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-  .then(() => console.log('MongoDB conectado!'))
-  .catch((err) => console.error('Erro na conexão:', err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Conectado ao MongoDB com sucesso!'))
+  .catch(err => console.error('Erro na conexão com o MongoDB:', err));
 
 app.use('/produto', produtoRoutes);
 
-app.listen(3000, () => console.log('Servidor online em http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('API de Produtos online. Use /produto');
+});
