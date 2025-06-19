9bd0f92a6e079726095f832ad8590ab54368cdcb
EnzoAlv <alvarengaenzo2005@gmail.com>
2025-06-19 00:03:55 -0300

    removido o a mensagem aplicada no ultimo commit. :(

--- index.js
+++ index.js
@@ -14,7 +14,3 @@ app.use('/produto', produtoRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-
-app.get('/', (req, res) => {
-  res.send('API de Produtos online. Use /produto');
-});
